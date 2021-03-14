38227970ef0f29a16ee13b2ce7deab66ce32239e
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-03-14 20:01:09 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if0009/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if0009/hitungbmi/ui/HitungFragment.kt
@@ -2,12 +2,12 @@ package org.d3if0009.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if0009.hitungbmi.R
 import org.d3if0009.hitungbmi.data.KategoriBmi
 import org.d3if0009.hitungbmi.databinding.FragmentHitungBinding
@@ -16,6 +16,21 @@ import java.util.zip.Inflater
 class HitungFragment : Fragment() {
     private lateinit var binding: FragmentHitungBinding
     private lateinit var kategoriBmi: KategoriBmi
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                    R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -30,7 +45,7 @@ class HitungFragment : Fragment() {
                     actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
