08d260553fe44f0f93797b63ec354d5a09abc5b9
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-04-26 13:52:16 +0700

    Menghapus data dari database

--- app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -7,6 +7,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.d3if0009.hitungbmi.db.BmiDao
 
-class HistoriViewModel(db: BmiDao) : ViewModel() {
+class HistoriViewModel(private val db: BmiDao) : ViewModel() {
     val data = db.getLastBmi()
+
+    fun hapusData() = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            db.clearData()
+        }
+    }
 }
