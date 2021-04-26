012784cbea1cbc82cf74ad0ceae3a86be8e09f86
Toifatul_Ngulum <toifatululum222@gmail.com>
2021-04-26 13:40:07 +0700

    Menampilkan data dalam bentuk list

--- app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriFragment.kt
@@ -1,48 +1,16 @@
 package org.d3if0009.hitungbmi.ui.histori
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
-
-
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import org.d3if0009.hitungbmi.databinding.FragmentHistoriBinding
 import org.d3if0009.hitungbmi.db.BmiDb
 
 class HistoriFragment : Fragment() {
-
-//    private val viewModel: HistoriViewModel by lazy {
-//        val db = BmiDb.getInstance(requireContext())
-//        val factory = HistoriViewModelFactory(db.dao)
-//        ViewModelProvider(this, factory).get(HistoriViewModel::class.java)
-//    }
-//
-//
-//    private lateinit var binding: FragmentHistoriBinding
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = FragmentHistoriBinding.inflate(
-//            layoutInflater,
-//            container, false
-//        )
-//        return binding.root
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        viewModel.data.observe(viewLifecycleOwner, {
-//            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
-//        })
-//    }
 
     private val viewModel: HistoriViewModel by lazy {
         val db = BmiDb.getInstance(requireContext())
@@ -51,18 +19,35 @@ class HistoriFragment : Fragment() {
     }
 
     private lateinit var binding: FragmentHistoriBinding
+    private lateinit var myAdapter: HistoriAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        binding = FragmentHistoriBinding.inflate(layoutInflater, container, false)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoriBinding.inflate(
+                layoutInflater,
+                container, false
+        )
+        myAdapter = HistoriAdapter()
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context,
+                    RecyclerView.VERTICAL))
+            adapter = myAdapter
+            setHasFixedSize(true)
+        }
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.data.observe(viewLifecycleOwner, {
-            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
+            binding.emptyView.visibility = if (it.isEmpty())
+                View.VISIBLE else View.GONE
+            myAdapter.updateData(it)
         })
     }
 }
--- app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriViewModel.kt
+++ app/src/main/java/org/d3if0009/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -1,6 +1,10 @@
 package org.d3if0009.hitungbmi.ui.histori
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.d3if0009.hitungbmi.db.BmiDao
 
 class HistoriViewModel(db: BmiDao) : ViewModel() {
